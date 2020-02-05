f4576297baccec6516ee9c2be707fb0d2e957ccf
Dol2oMo <p_sorasak@hotmail.com>
2020-02-05 23:19:02 +0700

    create code for test

---
+++ forecastData/Scene/SecondPage/SecondPageWorker.swift
@@ -0,0 +1,38 @@
+//
+//  SecondPageWorker.swift
+//  forecastData
+//
+//  Created by Sorasak on 12/16/19.
+//  Copyright © 2019 Dol2oMo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SecondPageWorker {
+
+    func getDataForcast7DayAgo(provice: String, completion: @escaping(_ result: JSON, _ error: String) -> ()){
+        
+        let url = "\(BaseUrl.baseUrl)/forecast/daily?q=\(provice)&cnt=10\(BaseUrl.appId)"
+        Alamofire.request(url).responseJSON { (response) in
+            
+            let statusCode = response.response?.statusCode ?? 0
+            switch response.result {
+            case .success(let result) :
+                
+                if statusCode == 200 {
+                    
+                    let json = JSON(result)
+                    completion("", "")
+                } else {
+                    
+                    completion("",response.error?.localizedDescription ?? "error_default_message")
+                }
+            case .failure(let error) :
+                
+                completion("",error.localizedDescription)
+            }
+        }
+    }
+}
